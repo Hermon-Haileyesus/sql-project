996b5ca2970e8f8f80d461845300ae7f8446677e
Hermon <hg2397@edu.turku.fi>
2025-03-01 20:52:19 +0200

    update tilavaraus

--- Tilavaraus.sql
+++ Tilavaraus.sql
@@ -1,3 +1,33 @@
+CREATE DATABASE tilavaraus;
+USE tilavaraus;
+
+-- Luodaan taulut
+CREATE TABLE tilat (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nimi VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE varaajat (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nimi VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE varaukset (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  tila INT,
+  varaaja INT,
+  varauspaiva DATE,
+  INDEX (tila),
+  INDEX (varaaja),
+  FOREIGN KEY (tila) REFERENCES tilat(id),
+  FOREIGN KEY (varaaja) REFERENCES varaajat(id)
+);
+
+-- Lisätään tilat
+INSERT INTO tilat (nimi) VALUES ('Auditorio'), ('A302'), ('A203');
+
+-- Lisätään varaajat
+INSERT INTO varaajat (nimi) VALUES ('Topi'), ('Vili'), ('Mikko');
 INSERT INTO varaukset (tila, varaaja, varauspaiva) VALUES
 ((SELECT id FROM tilat WHERE nimi = 'Auditorio'), (SELECT id FROM varaajat WHERE nimi = 'Topi'), '2025-01-15'),
 ((SELECT id FROM tilat WHERE nimi = 'A302'), (SELECT id FROM varaajat WHERE nimi = 'Vili'), '2025-01-20'),
